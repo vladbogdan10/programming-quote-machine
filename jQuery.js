e173575268c78d114447e735dc11470f8ac71fc5
Vlad Bogdan <vlad.bogdan90@gmail.com>
2017-07-12 11:07:29 +0200

    Changed .js file

---
+++ jQuery.js
@@ -0,0 +1,17 @@
+function renderHTML(json) {
+    $("#quoteText").hide().html(json.quoteText).fadeIn();
+    if (json.quoteAuthor === '') {
+        json.quoteAuthor = "Unknown";
+    }
+    $("#quoteAuthor").html(" - " + json.quoteAuthor);
+    var quote = $("#quoteText").text();
+    var author = $("#quoteAuthor").text();
+};
+
+$("#newQuote").on("click", function () {
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=renderHTML&callback=?");
+});
+
+$("#tweet").on("click", function () {
+    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent($("#quoteText").text() + $("#quoteAuthor").text()));
+});
